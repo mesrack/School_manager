c400df4dc49ba5fb51aca67e26566ccac41810ac
loic <loicfer@gmail.com>
2020-03-26 14:49:15 +0100

    add css files and update

--- public/js/header.js
+++ public/js/header.js
@@ -3,18 +3,8 @@ $("#dropdown").hover(
         $('.transition').toggleClass("visible");
         $('.transition').toggleClass("hidden");
         $('.transition').attr({
-            tabindex : "1",
+            tabindex : "-1",
             style : "display: block !important"
-    }), function () {
-        $('.transition').removeAttr('tabindex', "1");
-        $('.transition').removeAttr('style', "display: block !important");
-    }
+    })
 });
 
-
-$('.right.menu').on('click','.item', function(){
-    if(!$(this).hasClass('dropdown')) {
-        $(this).addClass('active').siblings('.item').removeClass('active');
-      }
- });
- 
